79b49ee0351d6f699ad16659068aa4fd589c6e71
asciphx <42582375+asciphx@users.noreply.github.com>
2021-06-30 20:21:48 +0800

    techempower

---
+++ examples/benchmark.cpp
@@ -0,0 +1,22 @@
+#include "crow.h"
+#include "mustache.h"
+#include <sstream>
+using namespace crow;using namespace std;
+int main() {
+  App<> app;app.set_directory("./static").set_types({"html","css","js","json"});
+ // app.route("/")([] {
+	//return (string)mustache::load("index.html");
+ // });
+  app.route("/plaintext")([] {
+	return "Hello, World!";
+  });
+  app.route("/json")([] {
+	json x;
+	x["message"]="Hello, World!";
+	return x;
+  });
+ // app.route("/fortune")([] {
+	//return "Hello, World!";
+ // });
+  app.loglevel(LogLevel::WARNING).port(8080).multithreaded().run();
+}
