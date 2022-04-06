31fc5dcd27e557977156271af1249e0575c6533c
asciphx <maxspore@foxmail.com>
2022-04-07 02:26:01 +0800

    update v1.1

--- include/cc/ci_map.h
+++ include/cc/ci_map.h
@@ -3,4 +3,5 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/functional/hash.hpp>
 #include <unordered_map>
-namespace cc { struct ci_hash { size_t operator()(const std::string& key) const { std::size_t seed = 0; std::locale locale; for (auto c : key) { boost::hash_combine(seed, std::toupper(c, locale)); } return seed; } }; struct ci_key_eq { bool operator()(const std::string& l, const std::string& r) const { return boost::iequals(l, r); } }; using ci_map = std::unordered_multimap<std::string, std::string, ci_hash, ci_key_eq>;}
+#include <string_view>
+namespace cc { struct ci_hash { size_t operator()(std::string key) const { std::size_t seed = 0;  for (auto c : key) { boost::hash_combine(seed, c);  } return seed; } }; struct ci_key_eq { bool operator()(std::string l, std::string r) const { return boost::iequals(l, r); } }; using ci_map = std::unordered_multimap<std::string, std::string, ci_hash, ci_key_eq>;}
