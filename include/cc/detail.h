f46e6b45b067405d19242726820e0cfc4b7304dd
asciphx <maxspore@foxmail.com>
2022-04-08 22:40:39 +0800

    update v1.2

--- include/cc/detail.h
+++ include/cc/detail.h
@@ -11,31 +11,7 @@
 #include "config.h"
 #include "cc/logging.h"
 namespace boost { namespace posix_time { class BOOST_SYMBOL_VISIBLE millseconds : public time_duration { public:template <typename T> BOOST_CXX14_CONSTEXPR explicit millseconds(T const& s, typename boost::enable_if<boost::is_integral<T>, void>::type* = BOOST_DATE_TIME_NULLPTR) : time_duration(0, 0, 0, numeric_cast<fractional_seconds_type>(s)) {} }; }}
-namespace cc { static std::unordered_map<uint64_t, std::string> RES_CACHE_MENU = {}; static std::unordered_map<uint64_t, int64_t> RES_CACHE_TIME = {}; static tm* RES_NOW; static auto RES_last = std::chrono::steady_clock::now(); tm now() { if (std::chrono::steady_clock::now() - RES_last < std::chrono::seconds(1)) return *RES_NOW; time_t rawtime; std::time(&rawtime);
-#if defined(_MSC_VER) || defined(__MINGW32__)
- localtime_s(RES_NOW, &rawtime);
-#else
- localtime_r(&rawtime, RES_NOW);
-#endif
- RES_last = std::chrono::steady_clock::now(); return *RES_NOW; } inline int64_t nowStamp(short& i) { if (std::chrono::steady_clock::now() - RES_last < std::chrono::seconds(1)) return mktime(RES_NOW) + i; time_t rawtime; std::time(&rawtime);
-#if defined(_MSC_VER) || defined(__MINGW32__)
- localtime_s(RES_NOW, &rawtime);
-#else
- localtime_r(&rawtime, RES_NOW);
-#endif
- RES_last = std::chrono::steady_clock::now(); return rawtime + i; } inline int64_t nowStamp(short&& i) { if (std::chrono::steady_clock::now() - RES_last < std::chrono::seconds(1)) return mktime(RES_NOW) + i; time_t rawtime; std::time(&rawtime);
-#if defined(_MSC_VER) || defined(__MINGW32__)
- localtime_s(RES_NOW, &rawtime);
-#else
- localtime_r(&rawtime, RES_NOW);
-#endif
- RES_last = std::chrono::steady_clock::now(); return rawtime + i; } inline int64_t nowStamp() { if (std::chrono::steady_clock::now() - RES_last < std::chrono::seconds(1)) return mktime(RES_NOW); time_t rawtime; std::time(&rawtime);
-#if defined(_MSC_VER) || defined(__MINGW32__)
- localtime_s(RES_NOW, &rawtime);
-#else
- localtime_r(&rawtime, RES_NOW);
-#endif
- RES_last = std::chrono::steady_clock::now(); return rawtime; } namespace detail { static std::string directory_ = STATIC_DIRECTORY;  class dumb_timer_queue { public: static unsigned short tick; dumb_timer_queue(boost::asio::io_service& io_service) : io_service_(io_service), deadline_timer_(io_service_) {
+namespace cc { static std::unordered_map<uint64_t, std::string> RES_CACHE_MENU = {}; static std::unordered_map<uint64_t, int64_t> RES_CACHE_TIME = {}; static tm* RES_NOW; static time_t RES_TIME_T; static std::string RES_DATE_STR; static auto RES_last = std::chrono::steady_clock::now(); tm now() { return *RES_NOW; } inline int64_t nowStamp(short& i) { return RES_TIME_T + i; } inline int64_t nowStamp(short&& i) { return RES_TIME_T + i; } inline int64_t nowStamp() { return RES_TIME_T; } namespace detail { static std::string directory_ = STATIC_DIRECTORY;  class dumb_timer_queue { public: static unsigned short tick; dumb_timer_queue(boost::asio::io_service& io_service) : io_service_(io_service), deadline_timer_(io_service_) {
 #ifdef _WIN32
  deadline_timer_.expires_from_now(boost::posix_time::millseconds(1));
 #else
